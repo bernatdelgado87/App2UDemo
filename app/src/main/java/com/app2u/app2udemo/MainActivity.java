6ab3272fcc80e5e86736c34b8dfbf684d5e65494
Bernat <Delgado>
2021-05-14 16:30:34 +0200

    create project

---
+++ app/src/main/java/com/app2u/app2udemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.app2u.app2udemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
